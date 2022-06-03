c45e8cec11fb26f4fa5fe22f1a91ddda5863515c
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-06-03 06:42:20 +0400

    Added javascript to the project

---
+++ js/shopping-cart.js
@@ -0,0 +1,6 @@
+(function () {
+    const shoppingBag = document.querySelector(".shopping-bag");
+    const addToCart = document.querySelector(".sale__items-item-button");
+  
+    shoppingBag.style.display = "none";
+  })();
